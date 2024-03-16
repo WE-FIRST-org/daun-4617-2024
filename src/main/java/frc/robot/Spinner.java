44808fbbfacee2849470a9fea6083eab1eef72b5
usymmij <usymmij@gmail.com>
2024-03-16 18:22:29 -0400

    spinner code

---
+++ src/main/java/frc/robot/Spinner.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+public class Spinner {
+    
+    private VictorSPX motor;
+
+    public Spinner() {
+        motor = new VictorSPX(9);
+        motor.configFactoryDefault();
+        motor.setNeutralMode(NeutralMode.Coast);
+    }
+
+    public void loop() {
+        motor.set(VictorSPXControlMode.PercentOutput, 0.5);
+    }
+}
